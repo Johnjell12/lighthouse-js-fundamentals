49acf5b2d292793e0f0dd1c2de7c6ae512f6eb5e
John Jell <leenaye20@gmail.com>
2021-11-10 21:15:59 -0500

    first commit

---
+++ conditionals.js
@@ -0,0 +1,25 @@
+const cold = false;
+if (cold) {
+    console.log("Make sure you pick out a scarf!");
+} else {
+    console.log("short sleeves are fine.");
+}
+const temperature = 15;
+if (temperature < 0) {
+    console.log("short sleeves won't cut it!");
+} else {
+    console.log("short sleeves are fine.");
+}
+console.log("Now you're ready to go outside!");
+
+const isCitizen = true;
+const age = 26;
+if (isCitizen && age > 18) {
+    
+}
+if (temperature < -40 || temperature > 40) {
+    console.log("Maybe going outside isn't such a great idea.");
+}
+if (!raining) {
+    console.log("Leave your umbrella at home!");
+}
